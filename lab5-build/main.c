cd38e1cc47cfa560f31cf96568f97046cc6204b7
Aleksandr Pavlov <av.pavlov@s-vfu.ru>
2017-11-17 12:56:17 +0900

    combinatorics

---
+++ lab5-build/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "combinatorics.h"
+
+extern int extern_count;
+int i_global_count;
+int global_count = 0;
+int main() {
+	int n, k, local_count;
+	local_count = i_global_count = extern_count = 0;
+	do {
+		printf("Input #%d: Enter n,k: ", ++local_count);
+		scanf("%d %d", &n, &k);
+		printf("C(%d,%d)=%d\n", n, k, C(n, k));
+	} while (n != 0 || k != 0);
+	return 0;
+}
